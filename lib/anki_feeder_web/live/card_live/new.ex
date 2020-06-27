d2c7965dfb964ea61e5ee1922a89e6d2d64b32f8
Julien Crevits <julien.crevits@gengo.com>
2020-06-27 13:05:13 +0900

    0.2.0 - highlight word in example + bug fixes + tweaks

--- lib/anki_feeder_web/live/card_live/new.ex
+++ lib/anki_feeder_web/live/card_live/new.ex
@@ -20,22 +20,29 @@ defmodule AnkiFeederWeb.CardLive.New do
 
   @impl true
   def handle_event("search-terms", %{"search" => search}, socket) do
-    IO.inspect(search, label: "search-terms")
     results = if search == "", do: nil, else: Mnemo.lookup_term(search)
 
-    if results && length(results) == 1 do
-      send(self(), {"use-term", List.first(results).id})
-    end
+    socket =
+      if results && length(results) == 1 do
+        prepare_card(socket, List.first(results).id)
+      else
+        socket
+      end
 
     socket =
-      assign(socket, term_results: results, search_term: search, examples: [], selected_term: nil)
+      assign(socket,
+        term_results: results,
+        search_term: search,
+        examples: [],
+        selected_term: nil,
+        selected_example: %{id: nil, japanese: nil, english: nil}
+      )
 
     {:noreply, socket}
   end
 
   @impl true
   def handle_event("use-term", %{"term_id" => term_id}, socket) do
-    IO.inspect(term_id, label: "use-term")
     {:noreply, prepare_card(socket, term_id)}
   end
 
@@ -43,21 +50,17 @@ defmodule AnkiFeederWeb.CardLive.New do
   def handle_event(
         "use-example",
         %{"example_id" => example_id, "japanese" => japanese, "english" => english},
-        socket
+        %{assigns: %{selected_term: selected_term}} = socket
       ) do
+    japanese = highlight_in_example(japanese, selected_term)
+
     selected_example = %Example{id: example_id, japanese: japanese, english: english}
 
     {:noreply, assign(socket, selected_example: selected_example)}
   end
 
   @impl true
-  def handle_event(
-        "save-card",
-        %{
-          "new_card" => new_card
-        },
-        socket
-      ) do
+  def handle_event("save-card", %{"new_card" => new_card}, socket) do
     card_details =
       Map.take(new_card, ["kanji", "reading", "definition", "ja_example", "en_example"])
 
@@ -75,15 +78,18 @@ defmodule AnkiFeederWeb.CardLive.New do
     end
   end
 
-  @impl true
-  def handle_info({"use-term", term_id}, socket) do
-    {:noreply, prepare_card(socket, term_id)}
-  end
-
   defp prepare_card(socket, term_id) do
     selected_term = Mnemo.get_term!(term_id)
     examples = Mnemo.lookup_examples(selected_term.kanji)
 
     assign(socket, selected_term: selected_term, examples: examples)
+  end
+
+  defp highlight_in_example(text, term) do
+    String.replace(
+      text,
+      term.kanji,
+      ~s(<b><font color="#fc0107">#{term.kanji}</font></b>)
+    )
   end
 end
