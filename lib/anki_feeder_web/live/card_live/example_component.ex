8e6712acdc06bb9f7702a757057c9e7abb1bdc50
Julien Crevits <julien.crevits@gengo.com>
2020-09-06 12:13:34 +0900

    dynamic CSS refactoring

--- lib/anki_feeder_web/live/card_live/example_component.ex
+++ lib/anki_feeder_web/live/card_live/example_component.ex
@@ -11,20 +11,24 @@ defmodule AnkiFeederWeb.Card.ExampleComponent do
 
       <%= for example <- @examples do %>
         <hr class="example-hr" />
-        <div class="row term-search-row <%= if @selected_example.id == to_string(example.id), do: ~s(selected-example) %> "
+        <div class="row term-search-row <%= selected_class(@selected_example, example) %>"
             phx-click="use-example"
             phx-value-example_id="<%= example.id %>"
             phx-value-japanese="<%= example.japanese %>"
             phx-value-english="<%= example.english %>" >
-          <div class="column">
+          <div class="row">
             <%= example.japanese %>
           </div>
-          <div class="column">
+          <div class="row">
             <%= example.english %>
           </div>
         </div>
       <% end %>
     </div>
     """
+  end
+
+  defp selected_class(selected_example, current_example) do
+    if selected_example.id == to_string(current_example.id), do: "selected-example"
   end
 end
