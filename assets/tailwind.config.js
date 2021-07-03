7984d3c989f6f9637284fa9c945d157a37d01c80
Julien <jcrevits@users.noreply.github.com>
2021-07-03 18:05:43 +0900

    Webpack 5 upgrade and general JS dependency update... (#10)
    * bump itertools
    
    * webpack 5 and JS deps upgrade, a bit of a mess...
    
    * update deploy script command
    
    * tailwind config file

---
+++ assets/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    "../lib/**/*.ex",
+    "../lib/**/*.leex",
+    "../lib/**/*.eex",
+    "./js/**/*.js",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
